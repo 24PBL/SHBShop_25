ca593c9b3bc45ec9a4ea1686cdec1148842870d9
pjh <onecushion4831@naver.com>
2025-05-04 15:03:46 +0900

    seller조회오류재수정

--- SHBShop/shbshop_back/controllers/book.py
+++ SHBShop/shbshop_back/controllers/book.py
@@ -93,7 +93,7 @@ def show_sbook_info(decoded_user_id, user_type, userId, shopId, bookId):
         return jsonify({"error": "잘못된 유저 유형"}), 404
     
     book = db.session.query(Sbooktrade).filter_by(bid=bookId).first()
-    shop = db.session.query(Shop).filter_by(sid=book.shopId).first()
+    shop = db.session.query(Shop).filter_by(sid=book.sid).first()
     seller = db.session.query(Commercial).filter_by(cid=shop.cid).first()
     
     print(f"book.shopId: {book.shopId}, request.shopId: {shopId}")
